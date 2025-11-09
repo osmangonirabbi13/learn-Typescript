7f52f08086d5a290a52b258cbf9516608de5ac62
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-11-09 18:58:35 +0600

    primitive data type

---
+++ Module1/PrimitiveType.ts
@@ -0,0 +1,15 @@
+// JS -> TS
+
+// string , number , boolean , null , undefined
+
+let userName : string = "Osman Goni " // string method 
+console.log(userName)
+
+let userId : number = 14  // number method 
+console.log(userId)
+
+let isMarrid : boolean = true // boolean method
+console.log(isMarrid)
+
+let name : undefined = undefined
+console.log(name)
