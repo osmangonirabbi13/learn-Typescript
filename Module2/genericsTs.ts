b5d57c22460251dccde5be2925bc80c9d180bfb1
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-11-11 22:42:25 +0600

    Generic type

---
+++ Module2/genericsTs.ts
@@ -0,0 +1,10 @@
+//dynamically generalize: Generic
+
+type  GenericArray<T> = Array<T>
+
+const friends :  GenericArray<string> = ["osman" , "goni" , "rabbi", "hi" , "hello"]
+console.log(friends)
+const num :  GenericArray<number> = [1,2,3,4,5,6,7,8,9]
+const bool :  GenericArray<boolean> = [true, false , true,false ,false]
+
+
